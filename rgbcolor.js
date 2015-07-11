d9721dd111b4f1507720dee4dea9fd511f452b23
leszko <rafal.leszko@gmail.com>
2015-07-11 17:01:28 +0200

    Refactor gradient.

--- rgbcolor.js
+++ rgbcolor.js
@@ -29,22 +29,34 @@ RgbColor.prototype.toString = function() {
 	return "#" + toHex(this.red) + toHex(this.green) + toHex(this.blue); 
 }
 
-RgbColor.prototype.applyGradientFrom = function(rgbColor) {
-	if (this.red < rgbColor.red) {
-		this.red = this.red + 1;
-	} else if (this.red > rgbColor.red) {
-		this.red = this.red - 1;
+RgbColor.prototype.gradientFrom = function(rgbColor) {
+	var result = this.clone();
+
+	if (result.red < rgbColor.red) {
+		result.red = result.red + 1;
+	} else if (result.red > rgbColor.red) {
+		result.red = result.red - 1;
 	}
 
-	if (this.green < rgbColor.green) {
-		this.green = this.green + 1;
-	} else if (this.green > rgbColor.green) {
-		this.green = this.green - 1;
+	if (result.green < rgbColor.green) {
+		result.green = result.green + 1;
+	} else if (result.green > rgbColor.green) {
+		result.green = result.green - 1;
 	}
 
-	if (this.blue < rgbColor.blue) {
-		this.blue = this.blue + 1;
-	} else if (this.blue > rgbColor.blue) {
-		this.blue = this.blue - 1;
+	if (result.blue < rgbColor.blue) {
+		result.blue = result.blue + 1;
+	} else if (result.blue > rgbColor.blue) {
+		result.blue = result.blue - 1;
 	}
+	
+	return result;
+}
+
+RgbColor.prototype.equals = function(rgbColor) {
+	return this.red == rgbColor && this.green == rgbColor.green && this.blue == rgb.blue;
+}
+
+RgbColor.prototype.clone = function() {
+	return new RgbColor(this.toString());
 }
